e851be5d595d0f4a3821c6a00c4d1705bca30ef5
Arthur Dantas <arthurdantaspro@gmail.com>
2023-06-21 07:55:18 -0300

    Implementação Conta Finalizada

--- lapa-backend/src/main/java/com/agrovetlapa/lapabackend/controller/ContaController.java
+++ lapa-backend/src/main/java/com/agrovetlapa/lapabackend/controller/ContaController.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/contas")
+@RequestMapping(value = "/contagits")
 public class ContaController {
     @Autowired
     ContaService contaService;
